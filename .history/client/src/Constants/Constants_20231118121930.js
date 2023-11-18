90e56895b6a930ca3d435c1f3725b0d474916ad0
rishikarajeev <rishjb@gmail.com>
2023-11-18 12:23:38 +0530

    .env changed with render and npm run build

---
+++ .history/client/src/Constants/Constants_20231118121930.js
@@ -0,0 +1,2 @@
+// export const BASE_URL="http://localhost:8081/api/roohnruca";
+https://rooh-n-ruca.onrender.com
---
+++ .history/client/src/Constants/Constants_20231118122000.js
@@ -0,0 +1,3 @@
+// export const BASE_URL="http://localhost:8081/api/roohnruca";
+export const BASE_URL="https://rooh-n-ruca.onrender.com/api/roohn";
+
