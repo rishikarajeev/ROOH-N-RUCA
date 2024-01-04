bdc0f0afbabf754c770274a5a9082ffb4ed77555
rishikarajeev <rishjb@gmail.com>
2024-01-04 13:00:16 +0530

    Base url update

--- .history/client/src/Constants/Constants_20231118121927.js
+++
@@ -1,2 +0,0 @@
-// export const BASE_URL="http://localhost:8081/api/roohnruca";
-
--- .history/client/src/Constants/Constants_20231118121958.js
+++
@@ -1,3 +0,0 @@
-// export const BASE_URL="http://localhost:8081/api/roohnruca";
-export const BASE_URL="https://rooh-n-ruca.onrender.com/api/roo";
-
